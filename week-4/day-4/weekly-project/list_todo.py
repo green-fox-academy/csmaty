3ca1b595cf4fc489daa511749b699d5b39767489
Matyas Csiki <matyas.csiki@gmail.com>
2015-12-03 14:37:42 +0100

    weekly project

---
+++ week-4/day-4/weekly-project/list_todo.py
@@ -0,0 +1,45 @@
+todolist = [
+        {'status': '[ ]', 'description': 'vegyel ezt' },
+        {'status': '[ ]', 'description': 'vegyel azt'},
+        {'status': '[ ]', 'description': 'vegyel amazt'},
+        {'status': '[ ]', 'description': 'vidd ki a szemetet'}
+
+]
+
+
+def itemlister(todolist):
+    n = 0
+    for i in todolist:
+        n += 1
+        print (str(n) + '. ' + i['status'], i['description'])
+
+
+def itemadder(todolist):
+
+    newitem = {'status': '[ ]', 'description': str(input("\nWrite the next todo item: \n"))}
+    todolist.append(newitem)
+
+def itemremover(todolist):
+
+    i = int(input('\nGive me the number of the item you want to remove:\n')) -1
+    todolist.pop(i)
+
+def taskcompleted(todolist):
+    i = int(input('\nGive me the number of the item completed:\n')) -1
+    todolist[i]['status'] = '[X]'
+
+
+
+
+
+
+
+
+
+itemlister(todolist)
+itemadder(todolist)
+itemlister(todolist)
+itemremover(todolist)
+itemlister(todolist)
+taskcompleted(todolist)
+itemlister(todolist)
